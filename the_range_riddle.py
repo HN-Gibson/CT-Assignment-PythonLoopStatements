22149eedc9096864bc51cdb9c91f5b40ac7c9729
H.N. Gibson <revhngibson@gmail.com>
2024-08-19 17:08:07 -0500

    edited to meet assignment parameters

--- the_range_riddle.py
+++ the_range_riddle.py
@@ -8,6 +8,6 @@ import random #imported random package
 moods = ["Happy","Sad","Energetic","Calm"] #set list for moods options
 days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] #set list for days of the week
 
-for day in days: # set the following code to run once for each day of the week
+for day in range(len(days)): # set the following code to run once for each day of the week
     mood = random.choice(moods) # set mood to be = a random choice from the list of moods for each iteration
-    print(f"On {day}, you we feeling {mood}.") #printed the statement of mood for each day before moving on to next iteration
+    print(f"On {days[day]}, you were feeling {mood}.") #printed the statement of mood for each day before moving on to next iteration
